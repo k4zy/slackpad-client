1788867e83c65828072635928109ccc67191a8a2
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2018-07-02 14:52:33 +0900

    Introduce react-navigation

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,21 +1,34 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
-    </View>
-  );
-};
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const MainStackNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+export default createDrawerNavigator({
+  Home: {
+    screen: MainStackNavigator,
   },
 });
