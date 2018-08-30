61f21c72d9da7c62ca362ba3aa16d0ddde597d4a
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2018-08-30 14:18:41 +0900

    Fixup endpoint

--- src/repository/Endpoint.ts
+++ src/repository/Endpoint.ts
@@ -1,2 +1,2 @@
-export const ENDPOINT_IP = '54.238.223.220';
-export const API_ENDPOINT = `http://${ENDPOINT_IP}`;
+export const ENDPOINT_IP = 'xn--edkuc.xn--fow072g.jp';
+export const API_ENDPOINT = `https://${ENDPOINT_IP}`;
